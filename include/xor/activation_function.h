86733197a704b14edce726225aa8397213bac332
lusanda <lusandanyilenda@gmail.com>
2026-07-04 22:32:16 +0200

    feat: implement sigmoid and relu activation functions

--- include/xor/activation_function.h
+++ include/xor/activation_function.h
@@ -1,9 +1,14 @@
 #ifndef ACTIVATION_FUNCTION_H
 #define ACTIVATION_FUNCTION_H
 
+#include <cmath>
+
 using namespace std;
 
 class ActivationFunction {
+    private:
+        static const float e;
+
     public:
         static float sigmoid(float input);
         static float relu(float input);
--- src/activation_function.cpp
+++ src/activation_function.cpp
@@ -1,9 +1,11 @@
 #include "xor/activation_function.h";
 
-float ActivationFunction::sigmoid(float input) {
+const float ActivationFunction::e = exp(1.0f);
 
+float ActivationFunction::sigmoid(float input) {
+    return (1/(1 + pow(e, (-input))));
 }
 
 float ActivationFunction::relu(float input) {
-    
+    return (input > 0.0f? input: 0.0f);
 }
