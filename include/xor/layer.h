bb619989a32fa6ce21406d22b2344c33c58dd4c7
lusanda <lusandanyilenda@gmail.com>
2026-07-05 17:35:38 +0200

    feat: initial implementation of Layer functionality

--- include/xor/layer.h
+++ include/xor/layer.h
@@ -9,16 +9,24 @@ using namespace std;
 
 class Layer {
     private:
+        //Layer& previousLayer;
         vector<float> currentInputs;
         vector<Node> nodes;
-        vector<float> currentOuputs;
+        vector<float> currentOutputs;
 
+        int numOfNodes;
         afs chosenActivationFunction;
         RandomNumber rng;
+
+        void addNode(int num);
     public:
-        Layer(afs activationFunction, int numOfNodes);
+        Layer();
+        Layer(int numOfNodes, afs activationFunction, int numOfInputs);
+        Layer(int numOfNodes, Layer& newPreviousLayer);
         void loadInputs(vector<float>& newInput);
         void loadActivationFunction(afs newActivationFunction);
+        afs getActivationFunction();
+        int getNumOfNodes();
         vector<float> outputs();
         ~Layer();
 
---
+++ src/layer.cpp
@@ -0,0 +1,59 @@
+#include "xor/layer.h"
+
+Layer::Layer() {
+    
+}
+
+
+Layer::Layer(int numOfNodes, afs activationFunction, int numOfInputs) {
+    chosenActivationFunction  = activationFunction;
+    currentInputs.assign(numOfInputs, 0);
+}
+
+Layer::Layer(int numOfNodes, Layer& newPreviousLayer) {
+    chosenActivationFunction  = newPreviousLayer.getActivationFunction();
+    currentInputs.clear();
+    currentInputs = newPreviousLayer.outputs();
+
+    addNode(numOfNodes);
+}
+
+void Layer::addNode(int num) {
+    for (size_t i = 1; i <= num; i++) {
+        Node newNode;
+
+        newNode.loadBias(rng.get());
+
+        for (size_t i = 1; i <= num; i++) {
+            float newWeight = rng.get();
+            newNode.addWeight(newWeight);
+        }
+
+        newNode.loadInputs(currentInputs);
+
+        newNode.loadActivationFunction(chosenActivationFunction);
+    }
+}
+
+void Layer::loadInputs(vector<float>& newInput) {
+    currentInputs.clear();
+    currentInputs = newInput;
+}
+
+void Layer::loadActivationFunction(afs newActivationFunction) {
+    chosenActivationFunction = newActivationFunction;
+}
+
+afs Layer::getActivationFunction() {
+    return chosenActivationFunction;
+}
+
+vector<float> Layer::outputs() {
+    currentOutputs.clear();
+
+    for (Node& node: nodes) {
+        currentOutputs.push_back(node.output());
+    }
+
+    return currentOutputs;
+}
